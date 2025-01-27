a068b7ec16cd17854d8814e29a634d118b4d9b78
DanielK-tech <dancek.kolegar@gmail.com>
2025-01-27 14:19:22 +0100

    up

--- my-react-app/src/components/MainContent.tsx
+++ my-react-app/src/components/MainContent.tsx
@@ -8,10 +8,22 @@ const Main: React.FC = () => {
     return (
         <section className="HomeSection" id="Home" tabIndex={0}>
             <div className="container">
-                <h1>SK Blind Guardians</h1>
-                <h3>Hiporehabilitace a aktivity s využitím koní.</h3>
-            </div> 
-            <OurService />  
+                <h1>
+                    Vítejte na stránkách spolku SK Blind Guardians - stáj
+                    Leskovec, z.s.
+                </h1>
+                <p>
+                    Jsme nezisková organizace zaměřená především na jezdecké
+                    aktivity s dětmi a mládeží a hiporehabilitační aktivity v
+                    pedagogické a sociální praxi a v kontaktních terapiích.
+                </p>
+                <p>S dětmi se sportovně věnujeme i hobby horsingu.</p>
+                <p>
+                    Naší specialitou je osvěta ve výcviku a jezdeckém využití
+                    zrakově znevýhodněných koní a poníků.
+                </p>
+            </div>
+            <OurService />
             <Foto />
         </section>
     );
